6b41ee74f83bfb2054b19ffc222e713e0ab5edc0
IOhannes m zmölnig <zmoelnig@iem.at>
2014-09-29 15:52:48 +0200

    set target distro to 'unstable'

--- debrelease.sh
+++ debrelease.sh
@@ -5,7 +5,7 @@ git checkout debian || exit 1
 # merge in 'master'
 git merge master || exit 1
 # update debian/changelog
-dch "new upstream" && dch -r
+dch -r "new upstream"
 git commit debian/changelog -m "ready for upload" || exit 1
 dpkg-buildpackage -rfakeroot || exit 1
 git checkout master
