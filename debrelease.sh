1c445ca69a191b3fd89d5c880025ba569360fc9f
IOhannes m zmölnig <zmoelnig@iem.at>
2014-09-29 15:59:49 +0200

    force distro:iem

--- debrelease.sh
+++ debrelease.sh
@@ -5,7 +5,7 @@ git checkout debian || exit 1
 # merge in 'master'
 git merge master || exit 1
 # update debian/changelog
-dch -i "new upstream"
+dch --force-distribution -D iem -i "new upstream"
 dch --force-distribution -D iem -r
 git commit debian/changelog -m "ready for upload" || exit 1
 dpkg-buildpackage -rfakeroot || exit 1
