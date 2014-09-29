5d57a9b03ed41d8e152b9800e4056ccc98e759cf
IOhannes m zmölnig <zmoelnig@iem.at>
2014-09-29 15:45:02 +0200

    fixed deb-script

--- debrelease.sh
+++ debrelease.sh
@@ -5,7 +5,7 @@ git checkout debian || exit 1
 # merge in 'master'
 git merge master || exit 1
 # update debian/changelog
-dch "new upstream"
-dch -r
-git commit debian/changelog -m "ready for upload"
-dpkg-buildpage -r fakeroot
+dch "new upstream" && dch -r
+git commit debian/changelog -m "ready for upload" || exit 1
+dpkg-buildpackage -rfakeroot || exit 1
+git checkout master
