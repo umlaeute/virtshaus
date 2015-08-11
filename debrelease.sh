f9b49aa4c5f896cf6e70a4c0e645f084cb72ea62
IOhannes m zmölnig <zmoelnig@iem.at>
2015-08-11 10:48:23 +0200

    Fix capitalisation

--- debrelease.sh
+++ debrelease.sh
@@ -5,8 +5,8 @@ git checkout debian || exit 1
 # merge in 'master'
 git merge master || exit 1
 # update debian/changelog
-dch --force-distribution -D iem -i "new upstream"
+dch --force-distribution -D iem -i "New upstream"
 dch --force-distribution -D iem -r
-git commit debian/changelog -m "ready for upload" || exit 1
+git commit debian/changelog -m "Ready for upload" || exit 1
 dpkg-buildpackage -rfakeroot || exit 1
 git checkout master
